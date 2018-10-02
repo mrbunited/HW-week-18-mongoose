7dc18d18c2ea76fdb09a543d68d9b7eef39cc37f
mradke2 <mrbunited@gmail.com>
2018-10-01 21:02:41 -0500

    scrapper setup

--- server.js
+++ server.js
@@ -3,30 +3,79 @@ var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cheerio = require("cheerio");
-var require = require("request");
-
-
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
-
-var PORT = 3000;
+var request = require("request");
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
 
+// Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+  
+  // Routes
+//   require("./routes/apiRoutes")(app);
+//   require("./routes/htmlRoutes")(app);
 // Configure middleware
 
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+
+// Require all models
+// var Article = require("./models/Article");
+// var Note = require("./models/Note");
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+            "Grabbing every thread name and link\n" +
+            "from reddit's webdev board:" +
+            "\n***********************************\n");
+
+// Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+request("http://old.reddit.com/r/webdev/", function(error, response, html) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(html);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // With cheerio, find each p-tag with the "title" class
+  // (i: iterator. element: the current element)
+  $("p.title").each(function(i, element) {
+
+    // Save the text of the element in a "title" variable
+    var title = $(element).text();
+
+    // In the currently selected element, look at its child elements (i.e., its a-tags),
+    // then save the values for any "href" attributes that the child elements may have
+    var link = $(element).children().attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+
+    // key:value
+
+    
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+});
+
+
+
+  // Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/week18HW");
